f3efb89c07f0dd6bb2ecb9bdddebc8f494677f5a
Pahutich <dpahutich@gmail.com>
2023-03-31 00:49:41 +0200

    added sound effect to medkit

--- Project Mako/Assets/Scripts/Medkit.cs
+++ Project Mako/Assets/Scripts/Medkit.cs
@@ -5,12 +5,28 @@ using UnityEngine;
 public class Medkit : MonoBehaviour, ICollectable
 {
     [SerializeField] private int amountToHeal;
+    private MeshRenderer meshRenderer;
+    private Collider hitBox;
+    private AudioSource audioSource;
+    private void Awake() {
+        meshRenderer = GetComponent<MeshRenderer>();
+        hitBox = GetComponent<Collider>();
+        audioSource = GetComponent<AudioSource>();
+    }
     public void Collect(Health health)
     {
-        health.GetHealthSystem().Heal(amountToHeal);
-        Destroy(gameObject);
+        StartCoroutine(OnCollect(health));
     }
 
+    private IEnumerator OnCollect(Health health)
+    {
+        health.GetHealthSystem().Heal(amountToHeal);
+        audioSource.Play();
+        meshRenderer.enabled = false;
+        hitBox.enabled = false;
+        yield return new WaitForSeconds(.5f);
+        Destroy(gameObject);
+    }
     private void OnTriggerEnter(Collider other) {
         var encounteredHealthEntity = other.GetComponent<Health>();
         if(encounteredHealthEntity)
