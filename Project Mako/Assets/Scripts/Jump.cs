caafa211b440bfa57adfc97c590666e192f08033
Pahutich <dpahutich@gmail.com>
2023-03-31 06:32:22 +0200

    added jump limit and altered car weight

--- Project Mako/Assets/Scripts/Jump.cs
+++ Project Mako/Assets/Scripts/Jump.cs
@@ -7,26 +7,38 @@ public class Jump : MonoBehaviour
     [SerializeField] private float jumpForce = 10f;
     private Rigidbody jumpingRigidbody;
     private PlayerInputActions playerInputActions;
-    private void Awake() {
+    [SerializeField] private float jumpFuelMax;
+    [SerializeField] private float fuelRegenerationAbility;
+    [SerializeField] private float jumpFuelCurrent;
+    private bool canRechargeFuel = true;
+    private void Awake()
+    {
         jumpingRigidbody = GetComponent<Rigidbody>();
         playerInputActions = new PlayerInputActions();
         playerInputActions.Player.Enable();
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+        jumpFuelCurrent = jumpFuelMax;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //bool isJumping = playerInputActions.Player.Jump.triggered;
         bool isJumping = playerInputActions.Player.Jump.ReadValue<float>() > 0.1f;
-        Debug.Log(isJumping);
-        if(isJumping)
+        bool hasSuffientAmountOfFuel = jumpFuelCurrent > 0;
+        if (isJumping && hasSuffientAmountOfFuel)
         {
             jumpingRigidbody.AddForce(transform.InverseTransformDirection(Vector3.up) * jumpForce, ForceMode.Force);
+            canRechargeFuel = false;
+            jumpFuelCurrent -= Time.deltaTime * fuelRegenerationAbility;
         }
+        else
+        {
+            canRechargeFuel = true;
+            jumpFuelCurrent += Time.deltaTime * fuelRegenerationAbility;
+        }
+            
+        if (jumpFuelCurrent > jumpFuelMax)
+            jumpFuelCurrent = jumpFuelMax;
+        if (jumpFuelCurrent < 0)
+            jumpFuelCurrent = 0;
     }
 }
