4f0a7c532afa546fc61564ee3a15e10141c7b633
Pahutich <dpahutich@gmail.com>
2023-04-07 15:35:34 +0200

    experimental car ai

---
+++ Project Mako/Assets/Scripts/EnemyVehicle.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyVehicle : MonoBehaviour
+{
+    [SerializeField] private float movementSpeed = 10f;
+    [SerializeField] private float rotationSpeed = 180f;
+    private Rigidbody enemyRigidbody;
+    private float translation, rotation;
+    private GameObject target;
+    private void Awake()
+    {
+        enemyRigidbody = GetComponent<Rigidbody>();
+        target = GameObject.FindGameObjectWithTag("Player");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!target)
+            return;
+
+        Vector3 targetDirection = transform.position - target.transform.position;
+        targetDirection.Normalize();
+
+        rotation = Vector3.Cross(targetDirection, transform.forward).y;
+    }
+
+    private void FixedUpdate()
+    {
+        enemyRigidbody.angularVelocity = rotationSpeed * rotation * new Vector3(0, 1, 0) * Time.deltaTime;
+        enemyRigidbody.velocity = transform.forward * movementSpeed;
+    }
+}
