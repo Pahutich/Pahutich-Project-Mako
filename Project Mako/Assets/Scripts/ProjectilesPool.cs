981a91d55174357b36753e82e1a3b92373618715
Pahutich <dpahutich@gmail.com>
2023-04-28 21:22:18 +0200

    addition of ProjectilesPool script now requires Shooter script

--- Project Mako/Assets/Scripts/ProjectilesPool.cs
+++ Project Mako/Assets/Scripts/ProjectilesPool.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Shooter))]
 public class ProjectilesPool : MonoBehaviour
 {
     private List<GameObject> pooledProjectiles = new List<GameObject>();
